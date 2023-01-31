efb580764498e8b07d4cc9ebe797831827a25acd
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-01-30 23:13:36 -0300

    adding the cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,8 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+
+  # set_cart is defined in the CurrentCart concern
+  before_action :set_cart, only: %i[ create ]
   before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
@@ -21,11 +25,12 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
+        format.html { redirect_to cart_url(@cart), notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
