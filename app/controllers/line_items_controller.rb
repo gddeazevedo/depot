a0085aad5178442d7b00a8e017988d9910a1d0ec
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-02-04 21:46:05 -0300

    adding quantity column to line_items table and fixing logic to add products to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,7 @@ class LineItemsController < ApplicationController
   # set_cart is defined in the CurrentCart concern
   before_action :set_cart, only: %i[ create ]
   before_action :set_line_item, only: %i[ show edit update destroy ]
-  before_action :reset_counter, only: %i[ create ]
+  after_action :reset_counter, only: %i[ create ]
 
   # GET /line_items or /line_items.json
   def index
@@ -28,11 +28,11 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@cart), notice: "Line item was successfully created." }
+        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
