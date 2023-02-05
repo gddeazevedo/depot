9b09cc26a490d9af7107f691590c3ae3ee83e174
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-02-05 18:33:25 -0300

    copying the product price into the line item row

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,11 +3,12 @@ class Cart < ApplicationRecord
 
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
-
+    
     unless current_item.nil?
       current_item.quantity += 1
+      current_item.product_unit_price = product.price
     else
-      current_item = line_items.build(product_id: product.id)
+      current_item = line_items.build(product_id: product.id, product_unit_price: product.price)
     end
 
     current_item
